27ce5dc7b52dd1e8e722b8e08ca77fc8aec60bdf
SarahIrons <sarahirons11@gmail.com>
2024-02-13 17:43:02 -0500

    Add project files.

---
+++ MethodAssignment/MultiplyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace MethodAssignment
+{
+    internal class MultiplyClass
+    {
+        public void MultiplyNumbers(int a, [Optional] int b)
+        {
+            int output = (a * 2) + (b * 2);
+            Console.WriteLine("Your number(s) times two, and then added together, is: " + output);
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+//MAIN METHOD ASSIGNMENT
+
+
+namespace MethodAssignment
+{
+    public class Program
+    {
+
+        private static void Main()
+        {
+            var myClass = new MultiplyClass(); // instatiate my class 
+            Console.WriteLine("Please enter a number here,  ");
+            int inputA = Int32.Parse(Console.ReadLine()); //gets input as a string and stores it, and converts to integer.
+            Console.WriteLine("Optionally pass another number here");
+            var inputB = Console.ReadLine(); // because we don't know if the user will input something we can't convert it right away
+            bool res;  // We use TryParse to check if we have a result that can be an int
+            int a;
+            res = int.TryParse(inputB, out a);
+            if (res) // Then we use that result as a conditional boolean to determine if we have an inputB to parse/run in the method
+            {
+                myClass.MultiplyNumbers(inputA, Int32.Parse(inputB)); 
+            } else {
+                myClass.MultiplyNumbers(inputA); //otherwise we run it with just inputA
+             }
+
+        }
+    }
+}
+
+
